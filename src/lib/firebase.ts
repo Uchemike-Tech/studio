067bb6a129a99314284136f0d49e60c769cc5973
MICHAEL UCHE <ucmike013@gmail.com>
2025-08-23 15:14:12 +0000

    FirebaseError: Failed to get document because the client is offline.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,17 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
+// IMPORTANT: This is a temporary solution for the demo environment.
+// In a real application, these keys should be stored in environment variables.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCf_lZafZ-z6dV-VPx6JXeYL37JEFHYkOY",
+  authDomain: "futo-clearance-portal.firebaseapp.com",
+  projectId: "futo-clearance-portal",
+  storageBucket: "futo-clearance-portal.appspot.com",
+  messagingSenderId: "546044918552",
+  appId: "1:546044918552:web:c3572fb1ff6495af463c6d"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
